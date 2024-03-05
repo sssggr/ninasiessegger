a6107e5c217f129cc64f74825a742d7f4df04dab
Nina Siessegger <n.siessegger@zeromail.org>
2024-03-05 12:53:12 +0100

    Update text

--- pages/agile-organisationsberatung.vue
+++ pages/agile-organisationsberatung.vue
@@ -1,7 +1,7 @@
 <script setup>
 useSeoMeta({
-  title: "Agile Organisationsentwicklung",
-  ogTitle: "Agile Organisationsentwicklung",
+  title: "Agile Organisationsberatung",
+  ogTitle: "Agile Organisationsberatung",
   ogDescription: "Als Organisationsentwicklerin unterstütze ich Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.",
   description: "Als Organisationsentwicklerin unterstütze ich Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.",
 })
@@ -14,11 +14,11 @@ useSeoMeta({
         Mein Angebot
       </h2>
       <h1 class="z-100">
-        Agile Organisationsentwicklung
+        Agile Organisationsberatung
       </h1>
-      <p class="z-100">Als Organisationsentwicklerin unterstütze ich Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.</p>
+      <p class="z-100">Ich unterstütze Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.</p>
       <p class="z-100">In meiner Arbeit ist es mir ein partizipativer Ansatz wichtig. Ich glaube daran, dass Veränderungen am nachhaltigsten sind, wenn sie gemeinschaftlich gestaltet werden. Daher arbeite ich eng mit Teams und Führungskräften zusammen, um die unterschiedlichen Perspektiven und Erfahrungen einzubeziehen.</p>
-      <p class="z-100">Mein Vorgehen ist geprägt von einer iterativen Herangehensweise. Veränderungen werden schrittweise und kontinuierlich umgesetzt, Feedback wird in den Entwicklungsprozess integriert. Dieser Ansatz der Organisationsentwicklung ermöglicht eine organische Entwicklung ohne den Bedarf an umfangreichen Transformationsprojekten. Das Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken und flexibel auf Veränderungen reagieren zu können.</p>
+      <p class="z-100">Mein Vorgehen ist geprägt von einer iterativen Herangehensweise. Veränderungen werden schrittweise und kontinuierlich umgesetzt, Feedback wird in den Entwicklungsprozess integriert. Dieser Ansatz der Organisationsberatung ermöglicht eine organische Entwicklung ohne den Bedarf an umfangreichen Transformationsprojekten. Das Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken und flexibel auf Veränderungen reagieren zu können.</p>
       <p class="z-100">Ich verwende Tools und Interventionen, die auf die spezifische Situation und den Kontext Ihrer Organisation abgestimmt sind. Denn ich bin der festen Überzeugung, dass es keinen universellen Ansatz gibt. Herausforderungen und Ziele sind immer spezifisch und erfordern individuelle Lösungsansätze.</p>
       <p class="z-100">Kontaktieren Sie mich gerne für ein unverbindliches Gespräch!</p>
     </div>
@@ -27,7 +27,7 @@ useSeoMeta({
 
 <script>
 export default {
-  name: 'agile-organisationsentwicklung',
+  name: 'agile-Organisationsberatung',
 }
 </script>
 <style scoped>
