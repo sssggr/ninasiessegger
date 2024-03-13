d61145ff8290be8e42d7e5e0ea301fe15ac6a07b
Nina Siessegger <n.siessegger@zeromail.org>
2024-03-13 15:57:39 +0100

    Update team development text

--- pages/teamentwicklung.vue
+++ pages/teamentwicklung.vue
@@ -14,22 +14,33 @@ useSeoMeta({
         Mein Angebot
       </h2>
       <h1 class="z-100">
-        Teamentwicklung
+        Agile Teamentwicklung
       </h1>
-      <p class="z-100">Teamentwicklung stärkt die Kommunikation, das Vertrauen und den Zusammenhalt im Team und ermöglicht effektive Zusammenarbeit. Mit individuell angepassten Impulsen, kreativen Methoden und agilem Ansatz ermöglicht Teamentwicklung nachhaltige Veränderungen. Zentral sind Anstöße zur Reflexion und Hilfe zur Selbsthilfe.</p>
-      <p class="z-100">Für neu zusammengesetzte Teams ist es zunächst wichtig aus einer Gruppe zum Team zu werden. In dieser Phase ist das Teambuilding zentral. Es geht darum, sich kennenzulernen, Vertrauen aufzubauen und Grundlegendes der Zusammenarbeit im Team zu klären.</p>
-      <p class="z-100">Schon länger bestehenden Teams begegnen in der Regel vielfältige Herausforderungen. Mögliche Anlässe für ein Teamcoaching sind:</p>
+      <h3 class="z-100">Mit innovativen Methoden und agilen Impulsen die Zukunft eurer Zusammenarbeit gestalten</h3>
+      <p class="z-100">Ihr wollt als Team etwas bewegen und gemeinsam weiter kommen? Teamentwicklung ist der Schlüssel, um die Kommunikation, das Vertrauen und den Zusammenhalt im Team zu stärken und die Grundlage für effektive Zusammenarbeit zu schaffen. Mit individuell zugeschnittenem Ansatz unterstütze ich euch dabei, nachhaltige Veränderungen zu bewirken.</p>
+      <p class="z-100">Kreative Methoden, agile Impulse und ein starker Fokus auf Reflexion und Selbsthilfe prägen meine Arbeit. So werden die im Team vorhandenen Ressourcen und Kompetenzen für alle Beteiligten sichtbar. Ich unterstütze dabei, diese bewusster einzusetzen und die Dialog- und Arbeitsfähigkeit nachhaltig zu fördern. Dabei setze ich auf partizipative Methoden. Denn ich bin überzeugt: Die Vielfalt der Perspektiven und die aktive Beteiligung aller Teammitglieder führen zu kreativen und guten Lösungen.</p>
+      <p class="z-100">Ich arbeite regelmäßig mit Remote-Teams zusammen und führe alle Teamentwicklungsprozesse auch online durch.</p>
+      <h2 class="z-100">Meine Leistungen</h2>
+      <h3 class="z-100">Agiles Arbeiten im Team</h3>
+      <p class="z-100">Teams in agilen Kontexten, die verstärkt auf Selbstorganisation setzen wollen, stehen vor besonderen Herausforderungen. Denn die bloße Einführung agiler Methoden oder Frameworks reicht oft nicht aus. Oftmals bleibt unklar, was Agilität und Selbstorganisation im Kern bedeuten und wie diese Prinzipien im Alltag konkret umgesetzt werden können.</p>
+      <p class="z-100">Die Dezentralisierung von klassischen Führungsaufgaben erfordert mehr Struktur und klare Regeln, um effektiv und produktiv zusammenarbeiten zu können. Ich unterstütze euch dabei einen guten Rahmen für eure Zusammenarbeit zu schaffen</p>
+      <p class="z-100">Gemeinsam können wir folgende Themen angehen:</p>
+      <p class="z-100"><b>Agile Methoden in der Praxis: </b>Ihr habt agile Methoden eingeführt, aber die Umsetzung hapert und stößt auf Unzufriedenheit? Wir analysieren die Herausforderungen und entwickeln gemeinsam praktikable Lösungen.</p>
+      <p class="z-100"><b>Gemeinsames Verständnis schaffen: </b>Euch fehlt ein klares Bild davon, was Agilität und Selbstorganisation für das Team bedeuten? Wir erarbeiten ein gemeinsames Verständnis und Leitbild.</p>
+      <p class="z-100"><b>Agiler Einstieg: </b>Ihr möchtet agil arbeiten, wisst aber nicht, wo ihr beginnen sollt und was für euren Kontext sinnvoll ist? Ich begleite euch bei der Einführung agiler Prinzipien und Praktiken.</p>
+      <p class="z-100"><b>Verantwortung übernehmen: </b>Ihr setzt auf Selbstorganisation und geteilte Führung, doch im Alltag wird wenig Verantwortung übernommen? Die Erarbeitung und Verteilung klarer Rollen und Aufgaben stärkt die Selbstorganisation im Team und führt dazu, dass Verantwortung übernommen werden kann.</p>
+      <p class="z-100"><b>Gemeinsam Entscheidungen treffen: </b> Ihr wollt Entscheidungen gemeinsam treffen, merkt aber, dass dies zu langwierigen und nervenaufreibenden Prozessen führt? Wir schauen uns an, wie ihr alle einbeziehen könnt und dennoch vorankommt.</p>
+      <h3 class="z-100">Team Kick-Off-Workshops</h3>
+      <p class="z-100">Für neu zusammengesetzte Teams ist es zentral eine gute Grundlage für die gemeinsame Zusammenarbeit zu schaffen. Im Rahmen eines Team Kick-Offs lernen sich alle Team-Mitglieder besser kennen, wir sprechen über eure gemeinsamen Ziele, klären Rollen im Team sowie die Spielregeln eurer Zusammenarbeit.</p>
+      <h3 class="z-100">Teamcoaching</h3>
+      <p class="z-100">Bestehende Teams, die ihre Zusammenarbeit verbessern möchten oder Lösungen für konkrete Herausforderungen finden wollen, begleite ich als Teamcoach. Mögliche Anlässe für ein Teamcoaching sind:</p>
       <ul class="z-100">
         <li>Verbesserung von Zusammenarbeit und Kommunikation</li>
-        <li>Unklare Rollenverteilung und Prozesse</li>
-        <li>Ein Wechsel in der Führung</li>
-        <li>Veränderung der Aufgaben</li>
-        <li>Arbeiten in hybriden Teams</li>
-        <li>Herausforderungen rund um Selbstorganisation und agiles Arbeiten</li>
+        <li>Klärung von Rollen im Team</li>
+        <li>Optimierung von Prozessen</li>
+        <li>Verbesserungen der Zusammenarbeit in remote oder hybriden Teams</li>
+        <li>Klärung von Spannungen und Konflikten</li>
       </ul>
-      <p class="z-100">In Teams kommt es immer wieder zu Konflikten, ich unterstütze in diesen Fällen als Konfliktmoderatorin oder Mediatorin. Es muss jedoch nicht immer erst zur Problemen kommen, um eine Teamentwicklungsmaßnahme anzustoßen. Gerne begleite ich Teams als Teamentwicklerin langfristig und arbeite kontinuierlich mit dem Team an der Verbesserung der Zusammenarbeit. Ich arbeite gerne mit Remote-Teams zusammen und führe alle Teamentwicklungsprozesse auch online durch.</p>
-      <p class="z-100">Mein Vorgehen behält dabei immer die im Team oder der Gruppe vorhandenen Ressourcen im Blick, um diese zu stärken und um die Dialog- und Arbeitsfähigkeit nachhaltig zu fördern.</p>
-      <p class="z-100">Kontaktieren Sie mich für ein unverbindliches Vorgespräch. So kann ich mir einen ersten Eindruck von Ihrem Anliegen und Ihren Zielen verschaffen. Daraufhin erstelle ich ein Angebot. Im nächsten Schritt spreche ich, wenn möglich, mit allen Mitgliedern des Teams, um alle Sichtweisen einzubeziehen und mir ein Gesamtbild zu verschaffen. Basierend darauf entwickle ich ein Konzept, dass es dem Team erlaubt Lösungswege für die vereinbarte Zielsetzung zu erarbeiten.</p>
     </div>
   </div>
 </template>
@@ -42,5 +53,13 @@ export default {
 <style scoped>
 .hg-marker .svg-container{
   margin-top: 5rem;
+}
+
+h3 {
+  margin-bottom: 2rem;
+}
+
+p ~ h3 {
+  margin-top: 2rem;
 }
 </style>
