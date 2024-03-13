268e00c9bd30f59788f0787cc89675457dc0c953
Nina Siessegger <n.siessegger@zeromail.org>
2024-03-13 15:17:04 +0100

    Update org consulting text

--- pages/agile-organisationsberatung.vue
+++ pages/agile-organisationsberatung.vue
@@ -2,8 +2,8 @@
 useSeoMeta({
   title: "Agile Organisationsberatung",
   ogTitle: "Agile Organisationsberatung",
-  ogDescription: "Als Organisationsentwicklerin unterstütze ich Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.",
-  description: "Als Organisationsentwicklerin unterstütze ich Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.",
+  ogDescription: "Als Organisationsberaterin unterstütze ich Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.",
+  description: "Als Organisationsberaterin unterstütze ich Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.",
 })
 </script>
 
@@ -16,11 +16,27 @@ useSeoMeta({
       <h1 class="z-100">
         Agile Organisationsberatung
       </h1>
-      <p class="z-100">Ich unterstütze Unternehmen dabei, Veränderungsprozesse zu gestalten. Dabei arbeite ich gerne mit agilen Methoden. Mein Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken.</p>
-      <p class="z-100">In meiner Arbeit ist es mir ein partizipativer Ansatz wichtig. Ich glaube daran, dass Veränderungen am nachhaltigsten sind, wenn sie gemeinschaftlich gestaltet werden. Daher arbeite ich eng mit Teams und Führungskräften zusammen, um die unterschiedlichen Perspektiven und Erfahrungen einzubeziehen.</p>
-      <p class="z-100">Mein Vorgehen ist geprägt von einer iterativen Herangehensweise. Veränderungen werden schrittweise und kontinuierlich umgesetzt, Feedback wird in den Entwicklungsprozess integriert. Dieser Ansatz der Organisationsberatung ermöglicht eine organische Entwicklung ohne den Bedarf an umfangreichen Transformationsprojekten. Das Ziel ist es, langfristig die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken und flexibel auf Veränderungen reagieren zu können.</p>
-      <p class="z-100">Ich verwende Tools und Interventionen, die auf die spezifische Situation und den Kontext Ihrer Organisation abgestimmt sind. Denn ich bin der festen Überzeugung, dass es keinen universellen Ansatz gibt. Herausforderungen und Ziele sind immer spezifisch und erfordern individuelle Lösungsansätze.</p>
-      <p class="z-100">Kontaktieren Sie mich gerne für ein unverbindliches Gespräch!</p>
+      <h3 class="z-100">Nachhaltige Veränderungsprozesse gestalten und zukunftsfähige Organisationen entwickeln</h3>
+      <p class="z-100">In einer Welt im Wandel sind Organisationen, die schnell auf Veränderungen reagieren können, wichtiger denn je. Die zunehmende Komplexität und Geschwindigkeit stellt Unternehmen und ihre Mitarbeitende vor neue Herausforderungen.</p>
+      <p class="z-100">Als Organisationsberaterin unterstütze ich insbesondere kleine und mittlere Unternehmen dabei, diesen Herausforderungen wirksam zu begegnen und sich weiterzuentwickeln.</p>
+      <h2 class="z-100">Mein Ansatz</h2>
+      <p class="z-100"><b>Partizipativ: </b>Ich glaube daran, dass Veränderungen am nachhaltigsten sind, wenn sie gemeinschaftlich gestaltet werden. Daher arbeite ich eng mit allen Stakeholdern zusammen, um die unterschiedlichen Perspektiven und Erfahrungen einzubeziehen.</p>
+      <p class="z-100"><b>Iterativ: </b>Veränderungen werden schrittweise und kontinuierlich umgesetzt, Feedback wird in den Entwicklungsprozess integriert. Dieser Ansatz der Organisationsentwicklung ermöglicht eine organische Entwicklung und bedarf keines umfangreichen Transformationsprojekts.</p>
+      <p class="z-100"><b>Individuell: </b>Ich verkaufe keine Frameworks oder bestimmte Methoden. Gemeinsam mit euch erarbeite ich individuelle Lösungen, die perfekt zu euren Herausforderungen und Zielen passen. Dabei bringe ich meine jahrelange Erfahrung in der Arbeit in und mit agilen Teams und Unternehmen ein.</p>
+      <p class="z-100"><b>Wirkungsvoll: </b>Mein Ziel ist es, die Fähigkeit zur kontinuierlichen Weiterentwicklung innerhalb der Organisation zu stärken und flexibel auf Veränderungen reagieren zu können.</p>
+      <h2 class="z-100">Meine Leistungen</h2>
+      <h3 class="z-100">Die Organisation weiterentwickeln</h3>
+      <p class="z-100">Euer Unternehmen befindet sich in einer starken Wachstums- oder Umbruchphase? Prozesse und Strukturen stoßen an ihre Grenzen? Ihr wollt dabei auf Teamarbeit und Selbstorganisation setzen?</p>
+      <p class="z-100">Das erreichen wir gemeinsam:</p>
+      <p class="z-100"><b>Mehr Klarheit: </b>Wir durchleuchten eure Entscheidungs- und Kommunikationswege und schärfen Rollen und Verantwortlichkeiten.</p>
+      <p class="z-100"><b>Notwendige Strukturen: </b>Ich unterstütze euch dabei, die richtigen Rahmenbedingungen für Selbstorganisation und Teamarbeit zu schaffen.</p>
+      <p class="z-100"><b>Neue Lösungsansätze: </b>Mit meinem frischen Blick von außen und neuen, innovativen Perspektiven auf eure Organisation finden wir gemeinsam neue Lösungsansätze.</p>
+      <h3 class="z-100"> Digitale Transformationsprozesse</h3>
+      <p class="z-100">Steht die Digitalisierung eines oder mehrerer Prozesse bei euch an? Ob ihr euer Wissensmanagement, eure Kommunikation oder das Kundenerlebnis neu und digital gestalten wollt, ich begleite euch bei der Gestaltung und Umsetzung digitaler Transformationsprozesse. Gemeinsam finden wir nachhaltige Lösungen.</p>
+      <p class="z-100">Das erreichen wir gemeinsam:</p>
+      <p class="z-100"><b>Höhere Akzeptanz: </b>Wir beziehen die relevanten Stakeholder ein und erreichen so eine hohe Akzeptanz der Veränderung.</p>
+      <p class="z-100"><b>Nachhaltigere Ergebnisse: </b>Die höhere Akzeptanz führt zu nachhaltigeren Ergebnissen und einer motivierteren Nutzung von neuen Prozessen und Strukturen.</p>
+      <p class="z-100"><b>Das beste Ergebnis: </b>Die Mitarbeitenden kennen die Prozesse und Herausforderungen am besten.</p>
     </div>
   </div>
 </template>
@@ -34,9 +50,18 @@ export default {
 .hg-marker .svg-container{
   margin-top: 3rem;
 }
+
 @media all and (max-width: 480px) {
-  .hg-marker .svg-container{
-  margin-top: 5rem;
+  .hg-marker .svg-container { 
+    margin-top: 5rem;
+  }
 }
+
+h3 {
+  margin-bottom: 2rem;
+}
+
+p ~ h3 {
+  margin-top: 2rem;
 }
 </style>
