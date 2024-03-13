c7d5e092b61909be38364d347261feec7795a310
Nina Siessegger <n.siessegger@zeromail.org>
2024-03-13 17:43:16 +0100

    Update text

--- pages/inqa-coaching.vue
+++ pages/inqa-coaching.vue
@@ -19,7 +19,7 @@ useSeoMeta({
     <div class="row">
       <div class="col-xs-12 col-md-8 col-lg-offset-1 col-lg-7">
         <p class="z-100">
-          Für viele Betriebe ist es eine Herausforderung, beim Tempo der Veränderungen der Arbeitswelt mitzuhalten. INQA-Coaching hilft Unternehmen mit bis zu 249 Beschäftigten, passgenaue Lösungen für die personalpolitischen und arbeitsorganisatorischen Veränderungsbedarfe im Zusammenhang mit der digitalen Transformation zu finden. Bis zu 80 Prozent der Beratungskosten können übernommen werden. Denn gerade kleinen und mittleren Unternehmen fehlen häufig die Mittel, um externe Beratung und Prozessbegleitung in Anspruch zu nehmen. Das Programm wird finanziert aus Mitteln des Europäischen Sozialfonds Plus (ESF Plus) und des Bundesministeriums für Arbeit und Soziales (BMAS).
+          Für viele Betriebe ist es eine Herausforderung, beim Tempo der Veränderungen der Arbeitswelt mitzuhalten. INQA-Coaching hilft Unternehmen mit bis zu 249 Beschäftigten, passgenaue Lösungen für die personalpolitischen und arbeitsorganisatorischen Veränderungsbedarfe im Zusammenhang mit der digitalen Transformation zu finden. Bis zu 80 Prozent der Beratungskosten können übernommen werden. Gerade kleinen und mittleren Unternehmen fehlen häufig die Mittel, um externe Beratung und Prozessbegleitung in Anspruch zu nehmen. Das Programm wird finanziert aus Mitteln des Europäischen Sozialfonds Plus (ESF Plus) und des Bundesministeriums für Arbeit und Soziales (BMAS).
         </p>
       </div>
       <div class="col-xs-7 col-xs-offset-2 col-md-offset-0 col-md-4 col-lg-3">
@@ -29,11 +29,11 @@ useSeoMeta({
     <div class="row">
       <div class="col-xs-12 col-md-12 col-lg-offset-1 col-lg-10">
         <p class="z-100">
-          Ich bin autorisierte und geprüfte INQA-Coach und begleite Sie gerne im Veränderungsprozess. Die Förderung umfasst maximal 12 Beratungstage. Bis zu 80 Prozent meiner Beratungskosten können durch das Programm finanziert werden.
+          Ich bin autorisierte und geprüfte INQA-Coach und begleite Unternehmen im Veränderungsprozessen. Die Förderung umfasst maximal 12 Beratungstage. Bis zu 80 Prozent meiner Beratungskosten können durch das Programm finanziert werden.
         </p>
         <h3>Welche Vorteile bietet INQA-Coaching?</h3>
         <p class="z-100">
-          INQA-Coaching bedient sich agiler Methoden und befähigt so Ihr Unternehmen zum eigenständigen agilen Arbeiten. Gleichzeitig ist das Programm durchgehend mitarbeiter*innenorientiert, das heißt, die Beschäftigten werden bei allen Schritten konsequent beteiligt. So entstehen passgenaue und nachhaltige Lösungen für Ihr Unternehmen, die von allen getragen werden und auch nach dem Ende des INQA-Coachings Bestand haben.
+          INQA-Coaching bedient sich agiler Methoden und befähigt Unternehmen zum eigenständigen agilen Arbeiten. Gleichzeitig ist das Programm durchgehend mitarbeiter*innenorientiert, das heißt, die Beschäftigten werden bei allen Schritten konsequent beteiligt. So entstehen passgenaue und nachhaltige Lösungen für Ihr Unternehmen, die von allen getragen werden und auch nach dem Ende des INQA-Coachings Bestand haben.
         </p>
         <h3>Wer wird gefördert?</h3>
         <p class="z-100">Das Förderprogramm richtet sich an in Deutschland ansässige KMU, die seit mindestens zwei Jahren am Markt sind, weniger als 250 Mitarbeitende beschäftigen und dabei mindestens eine Vollzeitkraft sozialversicherungspflichtig anstellen. Die Unternehmen sollten einen Jahresumsatz unter 50 Millionen Euro oder eine Bilanzsumme unter 43 Millionen Euro vorweisen und in den vergangenen drei Steuerjahren nicht mehr als 200.000 Euro an staatlichen Beihilfen, inklusive der INQA-Coaching-Förderung, erhalten haben.</p>
@@ -42,7 +42,7 @@ useSeoMeta({
     <div class="row">
       <div class="col-xs-12 col-md-8 col-lg-offset-1 col-lg-7">
         <h3>Ablauf und Förderschwerpunkte</h3>
-        <p class="z-100">Das INQA-Programm definiert einige große betriebliche Gestaltungsfelder. Ich unterstütze Sie als autorisierte INQA-Coach in folgenden Bereichen: </p>
+        <p class="z-100">Das INQA-Programm definiert einige große betriebliche Gestaltungsfelder. Ich unterstütze Unternehmen als autorisierte INQA-Coach in folgenden Bereichen: </p>
         <ul class="z-100">
             <li>Führung, berufliche Entwicklung & Karriere</li>
             <li>Arbeitsplatz der Zukunft, Arbeitszeit & Leistungspolitik</li> 
@@ -50,8 +50,8 @@ useSeoMeta({
             <li>Produktionsmodell & Arbeitsorganisation</li> 
             <li>Neue Geschäftsmodelle & Innovationsstrategien</li> 
         </ul>
-        <p class="z-100">Weitere Informationen zum Ablauf und den Förderschwerpunkten finden Sie im offiziellen Flyer (klicken Sie auf das Bild) und auf der  <a href="https://www.inqa.de/DE/handeln/inqa-coaching/uebersicht.html">INQA Website.</a></p>
-        <p class="z-100">Sind Sie neugierig geworden? Haben Sie Fragen zum INQA-Coaching? Kontaktieren Sie mich für ein unverbindliches Beratungsgespräch.</p>
+        <p class="z-100">Weitere Informationen zum Ablauf und den Förderschwerpunkten gibt es im offiziellen Flyer (ein Klick auf das Bild öffnet die Datei) und auf der  <a href="https://www.inqa.de/DE/handeln/inqa-coaching/uebersicht.html">INQA Website.</a></p>
+        <p class="z-100">Du möchtest mehr erfahren? Kontaktiere mich für ein unverbindliches Beratungsgespräch.</p>
       </div>
       <div class="col-xs-7 col-xs-offset-2 col-md-offset-0 col-md-4 col-lg-3">
         <a download="/inqa-flyer/teaser.jpg" href="https://login.inqa.de/fileadmin/PDF/Materialien/230619_Flyer_INQA-Coaching_digitale_bf_Coaches.pdf" title="INQA Flyer">
