4f9de4420cd9b6614a47af0ff56d121ca8b537ec
Nina Siessegger <n.siessegger@zeromail.org>
2024-03-22 16:12:33 +0100

    Update INQA Coaching

--- pages/inqa-coaching.vue
+++ pages/inqa-coaching.vue
@@ -12,14 +12,18 @@ useSeoMeta({
     <div class="row">
       <div class="col-xs-12 col-lg-offset-1 col-lg-10 col-md-12">
         <h1>
-          INQA-Coaching: Das geförderte Beratungsprogramm für kleine und mittlere Unternehmen (KMU)
+          INQA-Coaching in Hamburg
         </h1>
+        <h3 class="z-100">Agil in die digitale Zukunft mit dem INQA-Förderprogramm für KMU</h3>
       </div>
     </div>
     <div class="row">
       <div class="col-xs-12 col-md-8 col-lg-offset-1 col-lg-7">
         <p class="z-100">
-          Für viele Betriebe ist es eine Herausforderung, beim Tempo der Veränderungen der Arbeitswelt mitzuhalten. INQA-Coaching hilft Unternehmen mit bis zu 249 Beschäftigten, passgenaue Lösungen für die personalpolitischen und arbeitsorganisatorischen Veränderungsbedarfe im Zusammenhang mit der digitalen Transformation zu finden. Bis zu 80 Prozent der Beratungskosten können übernommen werden. Gerade kleinen und mittleren Unternehmen fehlen häufig die Mittel, um externe Beratung und Prozessbegleitung in Anspruch zu nehmen. Das Programm wird finanziert aus Mitteln des Europäischen Sozialfonds Plus (ESF Plus) und des Bundesministeriums für Arbeit und Soziales (BMAS).
+          Ich bin autorisierte und geprüfte INQA-Coach und begleite Unternehmen in und um Hamburg in Veränderungsprozessen. Die Förderung umfasst maximal 12 Beratungstage. Bis zu 80 Prozent meiner Beratungskosten können durch das Programm finanziert werden.
+        </p>
+        <p class="z-100">
+          Die Initiative Neue Qualität der Arbeit (INQA) ist ein Bundesprogramm für die Förderung kleiner und mittlerer Unternehmen sowie von Non-Profit-Organisationen. Beim INQA Coaching geht es darum, Unternehmen rund um das Thema Digitalisierung durch eine externe Prozessbegleitung zu unterstützen. Dabei wird auf Agilität und die Partizipation von Mitarbeitenden gesetzt.
         </p>
       </div>
       <div class="col-xs-7 col-xs-offset-2 col-md-offset-0 col-md-4 col-lg-3">
@@ -28,20 +32,17 @@ useSeoMeta({
     </div>
     <div class="row">
       <div class="col-xs-12 col-md-12 col-lg-offset-1 col-lg-10">
-        <p class="z-100">
-          Ich bin autorisierte und geprüfte INQA-Coach und begleite Unternehmen im Veränderungsprozessen. Die Förderung umfasst maximal 12 Beratungstage. Bis zu 80 Prozent meiner Beratungskosten können durch das Programm finanziert werden.
-        </p>
         <h3>Welche Vorteile bietet INQA-Coaching?</h3>
         <p class="z-100">
-          INQA-Coaching bedient sich agiler Methoden und befähigt Unternehmen zum eigenständigen agilen Arbeiten. Gleichzeitig ist das Programm durchgehend mitarbeiter*innenorientiert, das heißt, die Beschäftigten werden bei allen Schritten konsequent beteiligt. So entstehen passgenaue und nachhaltige Lösungen für Ihr Unternehmen, die von allen getragen werden und auch nach dem Ende des INQA-Coachings Bestand haben.
+          INQA-Coaching bedient sich agiler Methoden und befähigt Unternehmen zum eigenständigen agilen Arbeiten. Gleichzeitig bezieht das Programm durchgehend Mitarbeitende ein, das heißt, die Beschäftigten werden bei allen Schritten konsequent beteiligt. So entstehen passgenaue und nachhaltige Lösungen für Ihr Unternehmen, die breit getragen werden und auch nach dem Ende des INQA-Coachings Bestand haben.
         </p>
         <h3>Wer wird gefördert?</h3>
         <p class="z-100">Das Förderprogramm richtet sich an in Deutschland ansässige KMU, die seit mindestens zwei Jahren am Markt sind, weniger als 250 Mitarbeitende beschäftigen und dabei mindestens eine Vollzeitkraft sozialversicherungspflichtig anstellen. Die Unternehmen sollten einen Jahresumsatz unter 50 Millionen Euro oder eine Bilanzsumme unter 43 Millionen Euro vorweisen und in den vergangenen drei Steuerjahren nicht mehr als 200.000 Euro an staatlichen Beihilfen, inklusive der INQA-Coaching-Förderung, erhalten haben.</p>
       </div>
     </div>
     <div class="row">
-      <div class="col-xs-12 col-md-8 col-lg-offset-1 col-lg-7">
-        <h3>Ablauf und Förderschwerpunkte</h3>
+      <div class="col-xs-12 col-md-12 col-lg-offset-1 col-lg-10">
+        <h3>Was sind die Förderschwerpunkte?</h3>
         <p class="z-100">Das INQA-Programm definiert einige große betriebliche Gestaltungsfelder. Ich unterstütze Unternehmen als autorisierte INQA-Coach in folgenden Bereichen: </p>
         <ul class="z-100">
             <li>Führung, berufliche Entwicklung & Karriere</li>
@@ -50,12 +51,25 @@ useSeoMeta({
             <li>Produktionsmodell & Arbeitsorganisation</li> 
             <li>Neue Geschäftsmodelle & Innovationsstrategien</li> 
         </ul>
-        <p class="z-100">Weitere Informationen zum Ablauf und den Förderschwerpunkten gibt es im offiziellen Flyer (ein Klick auf das Bild öffnet die Datei) und auf der  <a href="https://www.inqa.de/DE/handeln/inqa-coaching/uebersicht.html">INQA Website.</a></p>
+        <h3>Wie läuft das INQA-Coaching ab?</h3>
+        <p class="z-100">Der Ablauf des INQA-Coachings gliedert sich in drei Phasen:</p>
+        <ol class="z-100">
+          <li>Initialphase (Dauer: ca. 1 Monat)</li>
+          <li>Innovationsphase ( Dauer: 3-4 Monate)</li>
+          <li>Lernphase (Dauer: 1 Monat)</li>
+        </ol>
+        <img alt="INQA Vorgehen" class="responsive-img" src="~/assets/images/inqa-vorgehen.jpg">
+        <p class="z-100">
+          <b>Initialphase:</b> In dieser Phase findet die Auftragsklärung statt, das zu bearbeitende Thema wird festgelegt und der Kick-off-Workshop wird durchgeführt. In dieser Phase werden Lab-Team und Lenkungskreis gebildet. 
+        </p>
+        <p class="z-100">
+          <b>Innovationsphase:</b> In diesem Abschnitt nimmt das Lab-Team seine Arbeit auf. Dieser Prozess besteht aus drei Phasen, die jeweils etwa vier Wochen dauern. Jeder Abschnitt startet mit einem Planungs-Workshop (ungefähr 4 Stunden) und endet mit einem Auswertungs-Workshop (ungefähr 1,5 Stunden). Die eigentliche Bearbeitung des gewählten Themas verbringt das Lab-Team in der Zeit dazwischen – überwiegend selbstorganisiert. Vorgesehen sind ca. zwei Stunden pro Woche in der Arbeitsphase, die jedes Teammitglied mindestens innerhalb der regulären Arbeitszeit aufwenden sollte, der*die Lab-Team-Verantwortliche mindestens 4 Stunden pro Woche.
+        </p>
+        <p class="z-100">
+          <b>Lernphase:</b> Diese Phase dient dazu, die Erfahrungen und erzielte Ergebnisse zu evaluieren. Dies geschieht im Rahmen eines Evaluations-Workshops.
+        </p>
+        <p class="z-100">Weitere Informationen zum Ablauf und den Förderschwerpunkten gibt es im <a href="https://www.inqa.de/SharedDocs/downloads/zusaetzliche-infos/inqa-flyer.pdf?__blob=publicationFile&v=1">offiziellen Flyer</a> und auf der <a href="https://www.inqa.de/DE/handeln/inqa-coaching/uebersicht.html">INQA Website.</a></p>
         <p class="z-100">Du möchtest mehr erfahren? Kontaktiere mich für ein unverbindliches Beratungsgespräch.</p>
-      </div>
-      <div class="col-xs-7 col-xs-offset-2 col-md-offset-0 col-md-4 col-lg-3">
-        <a download="/inqa-flyer/teaser.jpg" href="https://login.inqa.de/fileadmin/PDF/Materialien/230619_Flyer_INQA-Coaching_digitale_bf_Coaches.pdf" title="INQA Flyer">
-          <img alt="INQA Flyer" class="responsive-img" src="/inqa/teaser.jpg"></a>
       </div>
     </div>
   </div>
@@ -70,6 +84,10 @@ export default {
 <style scoped>
 .contact {
   margin-bottom: 1rem;
+}
+
+h3 {
+  margin-bottom: 2rem;
 }
 
 .contact p {
