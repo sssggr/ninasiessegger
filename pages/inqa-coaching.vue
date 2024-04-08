cd5b3c67354d9cab6c01b67aeb30c0e214bb2eef
exakte-aesthetik <post@exakteaesthetik.de>
2024-04-08 17:34:30 +0200

    inqa action

--- pages/inqa-coaching.vue
+++ pages/inqa-coaching.vue
@@ -10,41 +10,47 @@ useSeoMeta({
 <template>
   <div>
     <div class="row">
-      <div class="col-xs-12 col-lg-offset-1 col-lg-10 col-md-12">
+      <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
         <h1>
           INQA-Coaching in Hamburg
         </h1>
-        <h3 class="z-100">Agil in die digitale Zukunft mit dem INQA-Förderprogramm für KMU</h3>
       </div>
     </div>
     <div class="row">
-      <div class="col-xs-12 col-md-8 col-lg-offset-1 col-lg-7">
-        <p class="z-100">
+      <div class="col-lg-offset-1 col-lg-10 col-md-12">
+        <div class="row hero">
+          <div class="col-xs-12 col-md-6 col-sm-6 hero-item1">
+            <div class="hero-text-center-vertical">
+              <p>
+                Agil in die digitale Zukunft mit dem INQA-Förderprogramm für KMU
+              </p>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-6 col-sm-6 portrait-container hero-item2">
+            <img srcset="~/assets/images/inqa_header.png" alt="INQA Ilustration" class="responsive-img">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
+        <p>
           Die Initiative Neue Qualität der Arbeit (INQA) ist ein Bundesprogramm für die Förderung kleiner und mittlerer Unternehmen sowie von Non-Profit-Organisationen. Beim INQA Coaching geht es darum, Unternehmen rund um das Thema Digitalisierung durch eine externe Prozessbegleitung zu unterstützen. Dabei wird auf Agilität und die Partizipation von Mitarbeitenden gesetzt.
         </p>
-        <p class="z-100">
+        <p>
           Ich bin autorisierte und geprüfte INQA-Coach und begleite Unternehmen in und um Hamburg in Veränderungsprozessen. Die Förderung umfasst maximal 12 Beratungstage. Bis zu 80 Prozent meiner Beratungskosten können durch das Programm finanziert werden.
         </p>
-      </div>
-      <div class="col-xs-7 col-xs-offset-2 col-md-offset-0 col-md-4 col-lg-3">
-        <img src="~/assets/images/inqa-badge.jpg" alt="INQA Badge" class="responsive-img">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-md-12 col-lg-offset-1 col-lg-10">
         <h3>Welche Vorteile bietet INQA-Coaching?</h3>
-        <p class="z-100">
+        <p>
           INQA-Coaching bedient sich agiler Methoden und befähigt Unternehmen zum eigenständigen agilen Arbeiten. Gleichzeitig bezieht das Programm durchgehend Mitarbeitende ein, das heißt, die Beschäftigten werden bei allen Schritten konsequent beteiligt. So entstehen passgenaue und nachhaltige Lösungen für Ihr Unternehmen, die breit getragen werden und auch nach dem Ende des INQA-Coachings Bestand haben.
         </p>
         <h3>Wer wird gefördert?</h3>
-        <p class="z-100">Das Förderprogramm richtet sich an in Deutschland ansässige KMU, die seit mindestens zwei Jahren am Markt sind, weniger als 250 Mitarbeitende beschäftigen und dabei mindestens eine Vollzeitkraft sozialversicherungspflichtig anstellen. Die Unternehmen sollten einen Jahresumsatz unter 50 Millionen Euro oder eine Bilanzsumme unter 43 Millionen Euro vorweisen und in den vergangenen drei Steuerjahren nicht mehr als 200.000 Euro an staatlichen Beihilfen, inklusive der INQA-Coaching-Förderung, erhalten haben.</p>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-md-12 col-lg-offset-1 col-lg-10">
+        <p>
+          Das Förderprogramm richtet sich an in Deutschland ansässige KMU, die seit mindestens zwei Jahren am Markt sind, weniger als 250 Mitarbeitende beschäftigen und dabei mindestens eine Vollzeitkraft sozialversicherungspflichtig anstellen. Die Unternehmen sollten einen Jahresumsatz unter 50 Millionen Euro oder eine Bilanzsumme unter 43 Millionen Euro vorweisen und in den vergangenen drei Steuerjahren nicht mehr als 200.000 Euro an staatlichen Beihilfen, inklusive der INQA-Coaching-Förderung, erhalten haben.
+        </p>
         <h3>Was sind die Förderschwerpunkte?</h3>
-        <p class="z-100">Das INQA-Programm definiert einige große betriebliche Gestaltungsfelder. Ich unterstütze Unternehmen als autorisierte INQA-Coach in folgenden Bereichen: </p>
-        <ul class="z-100">
+        <p>Das INQA-Programm definiert einige große betriebliche Gestaltungsfelder. Ich unterstütze Unternehmen als autorisierte INQA-Coach in folgenden Bereichen: </p>
+        <ul>
             <li>Führung, berufliche Entwicklung & Karriere</li>
             <li>Arbeitsplatz der Zukunft, Arbeitszeit & Leistungspolitik</li> 
             <li>Soziale Beziehungen & Kultur</li> 
@@ -52,39 +58,66 @@ useSeoMeta({
             <li>Neue Geschäftsmodelle & Innovationsstrategien</li> 
         </ul>
         <h3>Wie läuft das INQA-Coaching ab?</h3>
-        <p class="z-100">Der Ablauf des INQA-Coachings gliedert sich in drei Phasen:</p>
-        <ol class="z-100">
+        <p>Der Ablauf des INQA-Coachings gliedert sich in drei Phasen:</p>
+        <ol>
           <li>Initialphase (Dauer: ca. 1 Monat)</li>
           <li>Innovationsphase ( Dauer: 3-4 Monate)</li>
           <li>Lernphase (Dauer: 1 Monat)</li>
         </ol>
-        <img alt="INQA Vorgehen" class="responsive-img" src="~/assets/images/inqa-vorgehen.jpg">
-        <p class="z-100">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-offset-1 col-lg-10 col-md-12">
+        <img srcset="~/assets/images/inqa_abb1.png" alt="INQA Ilustration" class="responsive-img inqa-illu">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
+        <p>
           <b>Initialphase:</b> In dieser Phase findet die Auftragsklärung statt, das zu bearbeitende Thema wird festgelegt und der Kick-off-Workshop wird durchgeführt. In dieser Phase werden Lab-Team und Lenkungskreis gebildet. 
         </p>
-        <p class="z-100">
+        <p>
           <b>Innovationsphase:</b> In diesem Abschnitt nimmt das Lab-Team seine Arbeit auf. Dieser Prozess besteht aus drei Phasen, die jeweils etwa vier Wochen dauern. Jeder Abschnitt startet mit einem Planungs-Workshop (ungefähr 4 Stunden) und endet mit einem Auswertungs-Workshop (ungefähr 1,5 Stunden). Die eigentliche Bearbeitung des gewählten Themas verbringt das Lab-Team in der Zeit dazwischen – überwiegend selbstorganisiert. Vorgesehen sind ca. zwei Stunden pro Woche in der Arbeitsphase, die jedes Teammitglied mindestens innerhalb der regulären Arbeitszeit aufwenden sollte, der*die Lab-Team-Verantwortliche mindestens 4 Stunden pro Woche.
         </p>
-        <p class="z-100">
+        <p>
           <b>Lernphase:</b> Diese Phase dient dazu, die Erfahrungen und erzielte Ergebnisse zu evaluieren. Dies geschieht im Rahmen eines Evaluations-Workshops.
         </p>
-        <p class="z-100">Weitere Informationen zum Ablauf und den Förderschwerpunkten gibt es im <a href="https://login.inqa.de/fileadmin/PDF/Materialien/230619_Flyer_INQA-Coaching_digitale_bf_Coaches.pdf">offiziellen Flyer</a> und auf der <a href="https://www.inqa.de/DE/handeln/inqa-coaching/uebersicht.html">INQA Website.</a></p>
-        <p class="z-100">Du möchtest mehr erfahren? Kontaktiere mich für ein unverbindliches Beratungsgespräch.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-offset-1 col-lg-10 col-md-12">
+        <img srcset="~/assets/images/inqa_abb2.png" alt="INQA Ilustration" class="responsive-img inqa-illu">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
+        <h3 class="less-margin">Welche Rollen gibt es im INQA-Coaching-Prozess?</h3>
+        <p>
+          INQA-Coaching arbeitet mit einem Rollenkonzept, das einen Lenkungskreis, ein Lab-Team und eine*n Verantwortliche*n des Lab-Teams vorsieht.
+        </p>
+        <p>
+          Der Lenkungskreis setzt sich aus Vertreterinnen und Vertretern der Geschäftsführung und der Beschäftigten zusammen, setzt die zentralen Themen und bewertet den Fortschritt des angestoßenen Veränderungsprozesses. 
+        </p>
+        <p>
+          Das Lab-Team setzt sich aus Beschäftigten der relevanten Organisationseinheiten zusammen. Es entwickelt und erprobt Gestaltungslösungen. Der*Die Lab-Team-Verantwortliche*r ist die zentrale Anlaufstelle für INQA-Coach und Lenkungskreis und treibt den Prozess im Lab-Team voran.
+        </p>
+        <p>
+          Empfohlene Besetzung des Lab-Teams: 3–6 Expert*innen aus den relevanten Bereichen, jedoch ohne Geschäftsführung. Falls es im Unternehmen eine mittlere Führungsebene gibt, kann diese im Lab-Team mitwirken, allerdings in der Rolle eines*einer fachlichen Expert*in, nicht als Führungskraft.
+        </p>
+        <p>Weitere Informationen zum Ablauf und den Förderschwerpunkten gibt es im <a href="https://login.inqa.de/fileadmin/PDF/Materialien/230619_Flyer_INQA-Coaching_digitale_bf_Coaches.pdf">offiziellen Flyer</a> und auf der <a href="https://www.inqa.de/DE/handeln/inqa-coaching/uebersicht.html">INQA Website.</a></p>
       </div>
     </div>
   </div>
 </template>
-
 <style scoped>
-.contact {
-  margin-bottom: 1rem;
+h3{
+  margin-top: 3rem;
 }
-
-h3 {
-  margin-bottom: 2rem;
+h3.less-margin{
+  margin-top: 2rem;
 }
-
-.contact p {
-  margin: 0;
+.inqa-illu{
+  margin-top: 2rem;
+  margin-bottom: 3rem;
 }
 </style>
--- pages/ueber-mich.vue
+++ pages/ueber-mich.vue
@@ -11,61 +11,90 @@ useSeoMeta({
   <div>
     <div class="row">
       <div class="col-xs-12">
-        <img src="~/assets/images/about_me.jpg" srcset="~/assets/images/about_me_2x.jpg 2x" alt="Portrait Nina Siessegger von Meike Kuether" class="responsive-img">
+        <img src="~/assets/images/about_me.jpg" srcset="~/assets/images/about_me_2x.jpg 2x"
+          alt="Portrait Nina Siessegger von Meike Kuether" class="responsive-img">
       </div>
     </div>
     <div class="row">
-    <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
-      <h1 class="z-100">
-       Über mich
-      </h1>
-      <p class="z-100">Ich bin 1980 geboren und lebe seit 2001 in Hamburg. Während meines Studiums der Geschichte, Politik und Osteuropastudien verschlug es mich in den Online-Medien-Bereich, wo ich fast 10 Jahre in unterschiedlichen Rollen - zuletzt als Online-Redakteurin beim NDR - tätig war.</p>
-      <p class="z-100">Seit 2014 bin ich in agilen Kontexten unterwegs - als Softwareentwicklerin, Tech Lead, Agile Coach, Beraterin, Autorin und Teamcoach. Zunächst für verschiedene Start-Ups, dann als Entwicklerin und Beraterin bei der Technologieberatung ThoughtWorks. Seit 2019 bin ich selbstständig tätig. Im Laufe der Jahre habe ich Erfahrungen in verschiedenen Firmen und Organisationen, vom kleinen Start-Up bis zum Großkonzern gesammelt und dabei in und mit internationalen und multikulturellen Teams gearbeitet.</p>
-      <p class="z-100">
-        Seit 2020 lege ich einen immer stärkeren Fokus auf die Begleitung von Organisationen und Teams in ihrer Weiterentwicklung und in Veränderungsprozessen. Mehr zu meiner Arbeitsweise habe ich 
-        <NuxtLink to="/wie-ich-arbeite">
-          hier zusammengefasst.
-        </NuxtLink>
-      </p>
-      <h2 class="z-100">Aus- und Weiterbildungen</h2>
-      <ul class="z-100">
-        <li>Zertifizierte Mediatorin nach Mediationsgesetz (IMK/EHV)</li>
-        <li>Systemisches Teamcoaching (die coachingakademie)</li>
-        <li>Soziokratie 3.0 Practitioner Level 1</li>
-        <li>Der agile Kulturwandel & Organsationsentwicklung (Teamworks)</li>
-        <li>Autorisierte Prozessberaterin im Programm INQA-Coaching</li>
-        <li>Certified ScrumMaster®</li>
-        <li>Verschiedene Weiterbildungen (Online Moderation, Visualisierung, Management von Remote Teams, Liberating Structures, Design Thinking)</li>
-      </ul>
+      <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
+        <h1>
+          Über mich
+        </h1>
+        <p>
+          Ich bin 1980 geboren und lebe seit 2001 in Hamburg. Während meines Studiums der Geschichte, Politik und
+          Osteuropastudien verschlug es mich in den Online-Medien-Bereich, wo ich fast 10 Jahre in unterschiedlichen
+          Rollen - zuletzt als Online-Redakteurin beim NDR - tätig war.
+        </p>
+        <p>
+          Seit 2014 bin ich in agilen Kontexten unterwegs - als Softwareentwicklerin, Tech Lead, Agile Coach, Beraterin,
+          Autorin und Teamcoach. Zunächst für verschiedene Start-Ups, dann als Entwicklerin und Beraterin bei der
+          Technologieberatung ThoughtWorks. Seit 2019 bin ich selbstständig tätig. Im Laufe der Jahre habe ich
+          Erfahrungen in verschiedenen Firmen und Organisationen, vom kleinen Start-Up bis zum Großkonzern gesammelt und
+          dabei in und mit internationalen und multikulturellen Teams gearbeitet.
+        </p>
+        <p>
+          Seit 2020 lege ich einen immer stärkeren Fokus auf die Begleitung von Organisationen und Teams in ihrer
+          Weiterentwicklung und in Veränderungsprozessen. Mehr zu meiner Arbeitsweise habe ich
+          <NuxtLink to="/wie-ich-arbeite">
+            hier zusammengefasst.
+          </NuxtLink>
+        </p>
+      </div>
     </div>
+    <section>
+    <div class="row">
+      <div class="col-lg-offset-1 col-lg-10 col-md-12">
+        <h2>
+          Aus- und Weiterbildungen
+        </h2>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
+        <ul>
+          <li>Zertifizierte Mediatorin nach Mediationsgesetz (IMK/EHV)</li>
+          <li>Systemisches Teamcoaching (die coachingakademie)</li>
+          <li>Soziokratie 3.0 Practitioner Level 1</li>
+          <li>Der agile Kulturwandel & Organsationsentwicklung (Teamworks)</li>
+          <li>Autorisierte Prozessberaterin im Programm INQA-Coaching</li>
+          <li>Certified ScrumMaster®</li>
+          <li>Verschiedene Weiterbildungen (Online Moderation, Visualisierung, Management von Remote Teams, Liberating
+            Structures, Design Thinking)</li>
+        </ul>
+      </div>
+    </div>
+  </section>
   </div>
 </template>
 
 <style scoped>
-  img{
-    margin-top: -5rem;
-    padding-bottom: 2.5rem;
-  }
-  @media (max-width: 1200px) {
-  footer .svg-container{
+img {
+  margin-top: -5rem;
+  padding-bottom: 2.5rem;
+}
+
+@media (max-width: 1200px) {
+  footer .svg-container {
     width: 306px;
   }
 }
+
 @media (max-width: 1024px) {
-  img{
+  img {
     margin-top: -3rem;
     padding-bottom: 2rem;
   }
 }
+
 @media (max-width: 768px) {
-  img{
+  img {
     margin-top: 0;
     padding-bottom: 2rem;
   }
 }
+
 @media (max-width: 480px) {
-  img{
+  img {
     margin-top: 0;
     padding-bottom: 2rem;
   }
