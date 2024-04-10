af5e522981f0a53d94913fe95422687e6172911e
Nina Siessegger <n.siessegger@zeromail.org>
2024-04-10 14:50:09 +0200

    Add sitemap module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['nuxt-simple-sitemap'],
+  sitemap: {
+    hostname: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://ninasiessegger.de',
+  },
   css: ['normalize.css/normalize.css', 'flexboxgrid', '@/assets/css/base.css'],
   app: {
     head: {
