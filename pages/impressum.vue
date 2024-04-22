298e31363ea7eb1f359b93baa8e2b1104ced3555
exakte-aesthetik <post@exakteaesthetik.de>
2024-04-22 14:54:40 +0200

    Kontakt aufgeräumt

--- pages/impressum.vue
+++ pages/impressum.vue
@@ -8,18 +8,18 @@ useSeoMeta({
 <template>
   <div class="row">
     <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
-      <h1 class="z-100">
+      <h1>
         Impressum
       </h1>
       <div class="contact">
-        <p class="z-100">Nina Siessegger</p>
-        <p class="z-100">Bernstorffstraße 120</p>
-        <p class="z-100">22767 Hamburg</p>
+        <p>Nina Siessegger</p>
+        <p>Bernstorffstraße 120</p>
+        <p>22767 Hamburg</p>
       </div>
       <p> 
         Umsatzsteuer-Identifikationsnummer gem. § 27a UStG: DE285651285
       </p>
-      <h3 class="z-100">
+      <h3>
         Kontakt
       </h3>
       <p>
@@ -27,29 +27,29 @@ useSeoMeta({
           hallo@ninasiessegger.de
         </a>
       </p>
-      <h3 class="z-100">
+      <h3>
         Bildnachweise
       </h3>
-      <p class="z-100">
+      <p>
         Portraitfotos von
         <a href="https://authentische-businessfotografie.de/">Meike Kuether</a>
       </p>
-      <h3 class="z-100">
+      <h3>
         Webdesign
       </h3>
-      <p class="z-100">
+      <p>
         <a href="https://www.exakteaesthetik.de"> Büro für Exakte Ästhetik </a>
       </p>
-      <h3 class="z-100">
+      <h3>
         Haftung für Inhalte
       </h3>
-      <p class="z-100">
+      <p>
         Die Inhalte meiner Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte kann ich jedoch keine Gewähr übernehmen. Als Diensteanbieter bin ich gemäß § 7 Abs. 1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Als Diensteanbieter bin ich jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Zu diesen übermittelten fremden Informationen zählen insbesondere die durch unsere Nutzer gestellten Anfragen nach dem Informationsfreiheitsgesetz sowie die Antworten der jeweiligen Behörden. Die Verpflichtung zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werde ich diese Inhalte umgehend entfernen.
       </p>
-      <h3 class="z-100">
+      <h3>
         Haftung für Links
       </h3>
-      <p class="z-100">
+      <p>
         Mein Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte ich keinen Einfluss haben. Deshalb kann ich für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werde ich derartige Links umgehend entfernen.
       </p>
     </div>
@@ -60,7 +60,6 @@ useSeoMeta({
 .contact {
   margin-bottom: 1rem;
 }
-
 .contact p {
   margin: 0;
 }
